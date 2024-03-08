92ea6804c12e09782ea123da35dd7714ab159d3a
Lillian Berge (CCI RPT RES1) <lilbe@st-linrgs632.st.statoil.no>
2024-03-08 11:02:26 +0100

    Revert "Hardcoding f version for test"
    This reverts commit 4407483cb73d6dc03786eba92cea066a24a3899b.

--- src/subscript/check_swatinit/check_swatinit.py
+++ src/subscript/check_swatinit/check_swatinit.py
@@ -718,8 +718,7 @@ def get_parser() -> argparse.ArgumentParser:
     parser.add_argument(
         "--version",
         action="version",
-#        version="%(prog)s (subscript version " + subscript.__version__ + ")",
-        version = "1.1.1_test"
+        version="%(prog)s (subscript version " + subscript.__version__ + ")",
     )
     return parser
 
